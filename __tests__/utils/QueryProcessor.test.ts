e75fcc80957243b7c9b472ba327fe48ec59e3ffe
Meghna Chityala <megchityala@meghnas-mbp.wifi.local.cmu.edu>
2023-09-15 12:41:41 -0400

    added what is your andrew ID

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,10 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return the Andrew ID response', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe("My Andrew ID is mchityal."); // Replace with the actual Andrew ID.
+    });
 });
